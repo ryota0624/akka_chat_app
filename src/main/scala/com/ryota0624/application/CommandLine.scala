8d5254ceeddc7bd6b7f258625318796f2db26867
ryota0624 <ryota.suzuki@kyash.co>
2020-05-21 20:54:04 +0900

    compile error修正

--- src/main/scala/com/ryota0624/application/CommandLine.scala
+++ src/main/scala/com/ryota0624/application/CommandLine.scala
@@ -5,7 +5,7 @@ import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorSystem, Behavior}
 import com.ryota0624.chat.ChatRooms
 import com.ryota0624.user.{LoggedInUser, User, Users}
-import com.ryota0624.{ApplicationTime, user}
+import com.ryota0624.{ApplicationTime, Response, user}
 
 
 object ChatApplication {
@@ -16,18 +16,18 @@ object ChatApplication {
 
   final case class ChatRoomsCommand(command: ChatRooms.Command) extends Command
 
-  final case class Response(msg: Users.Response) extends Command
+  final case class UsersResponse(msg: Response[Users.Document]) extends Command
 
   def apply()(implicit time: ApplicationTime): Behavior[Command] = Behaviors.setup { context =>
-    context.self ! ChatApplication.UsersCommand(Users.RegisterAnonymousUser)
-    context.self ! ChatApplication.UsersCommand(Users.RegisterAnonymousUser)
+    val usersResponseAdapter = context.messageAdapter(UsersResponse)
+    context.self ! ChatApplication.UsersCommand(Users.RegisterAnonymousUser(usersResponseAdapter))
+    context.self ! ChatApplication.UsersCommand(Users.RegisterAnonymousUser(usersResponseAdapter))
     context.self ! ChatApplication.UsersCommand(Users.RegisterUser(
       User.Name("suzuki"),
       new LoggedInUser.Email("hoge"),
       new user.LoggedInUser.Password("huga"),
-      context.messageAdapter(Response)
-    )
-    )
+      usersResponseAdapter
+    ))
 
     def startSubscriber(): Behavior[Any] = Behaviors.setup { subCtx =>
       subCtx.system.eventStream.tell(Subscribe(subCtx.self))
@@ -47,7 +47,7 @@ object ChatApplication {
       case ChatRoomsCommand(command) =>
         chatRooms ! command
         Behaviors.same
-      case Response(msg) =>
+      case UsersResponse(msg) =>
         context.log.info(msg.toString)
         Behaviors.same
     }
